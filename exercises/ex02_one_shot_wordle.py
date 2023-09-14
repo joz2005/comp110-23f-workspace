c2acd8096bdbb8f95a334fdffc58cb79e9d31db7
Joseph Zheng <jozheng@unc.edu>
2023-09-14 11:40:01 -0400

    Added ex02

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,37 @@
+__author__ = "730655251"
+
+secret_word = "python"
+user = str(input(f"What is your {len(secret_word)}-letter guess? "))
+
+while len(user) != len(secret_word):
+    user = str(input(f"That was not {len(secret_word)} letters! Try again: "))
+
+index = 0
+box_string = ""
+
+while index < len(user):
+
+    letter_found = False
+    letter_index = 0
+
+    if user[index] == secret_word[index]:
+        box_string += "\U0001F7E9"
+
+    else:
+        while not letter_found and letter_index < len(secret_word):
+            if user[index] == secret_word[letter_index]:
+                letter_found = True
+            letter_index += 1
+        if letter_found:
+            box_string += "\U0001F7E8"
+        else:
+            box_string += "\U00002B1C"
+            
+    index += 1
+
+print(box_string)
+
+if user.lower() == secret_word:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
